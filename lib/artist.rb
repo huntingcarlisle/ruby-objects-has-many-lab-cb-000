174414961895289c8e17448770b1473eb482a75c
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-21 18:10:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name, :songs
+  @@all_songs = []
   def initialize(name)
     @name = name
     @songs = []
@@ -7,12 +8,17 @@ class Artist
   
   def add_song(song)
     @songs << song
+    @@all_songs << new_song
   end
   
   def add_song_by_name(name)
     new_song = Song.new(name)
     new_song.artist = self
     @songs << new_song
+    @@all_songs << new_song
   end
+  
+  def self.song_count
+    return @@all_songs
   
 end
