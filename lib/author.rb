8e02504b2c3f98e692f87a2f533914ff27627402
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-21 18:22:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,22 @@
 class Author
 
-  attr_accessor :name
+  attr_accessor :name, :posts
+  @@all_posts = []
   
-  def init
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@all_posts << post
+  end
+  
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    self.add_post(new_post)
+  end
 
 end
