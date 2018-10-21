2bb378367b685887c346c33d75a66ebbbc46b29c
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-21 18:17:50 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,9 +14,7 @@ class Artist
   
   def add_song_by_name(name)
     new_song = Song.new(name)
-    new_song.artist = self
-    @songs << new_song
-    @@all_songs << new_song
+    self.add_song(new_song)
   end
   
   def self.song_count
